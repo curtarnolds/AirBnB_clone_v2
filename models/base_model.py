62730503d77f007af059091a78038ebfc14c1670
curtarnolds <afayedormawuli@gmail.com>
2023-08-24 19:27:37 +0000

    Fixed pycodestyle errors in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,13 +2,13 @@
 """
 Contains class BaseModel
 """
-
 from datetime import datetime
 import models
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from os import getenv
+
 
 time_fmt = "%Y-%m-%dT%H:%M:%S.%f"
 
