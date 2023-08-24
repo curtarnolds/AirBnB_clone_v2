b1e0efdfb73e9cb8799b384b3f4306640a75b6be
curtarnolds <afayedormawuli@gmail.com>
2023-08-24 20:54:34 +0000

    Fixed pycodestyle errors in user.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import hashlib
-import models
+import hashlib  # noqa
+import models  # noqa
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
@@ -9,17 +9,18 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-      """MySQL database user. Inherits from SQLAlchemy Base and links to the MySQL table users.
-        Attributes:
-        __tablename__ (str): The name of the MySQL table to store users.
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
-      """ 
-      if getenv('HBNB_TYPE_STORAGE') == 'db':
+    """MySQL database user. Inherits from SQLAlchemy Base and links to the
+    MySQL table users.
+      Attributes:
+      __tablename__ (str): The name of the MySQL table to store users.
+      email: (sqlalchemy String): The user's email address.
+      password (sqlalchemy String): The user's password.
+      first_name (sqlalchemy String): The user's first name.
+      last_name (sqlalchemy String): The user's last name.
+      places (sqlalchemy relationship): The User-Place relationship.
+      reviews (sqlalchemy relationship): The User-Review relationship.
+    """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
         email = Column(String(128),
                        nullable=False)
@@ -36,7 +37,7 @@ class User(BaseModel, Base):
         reviews = relationship("Review",
                                backref="user",
                                cascade="all, delete-orphan")
-      else:
+    else:
         email = ""
         _password = ""
         first_name = ""
@@ -54,5 +55,3 @@ class User(BaseModel, Base):
         def password(self, pwd):
             """hashing password values"""
             self._password = pwd
-
-     
