5fa752ea2aa86a6c0192c9aa808f15fb51cac724
curtarnolds <afayedormawuli@gmail.com>
2023-08-24 20:07:56 +0000

    Fixed pycodestyle errors in db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,7 @@
 """Defines the DBStorage engine."""
 from os import getenv
 from models.base_model import Base
-from models.base_model import BaseModel
+from models.base_model import BaseModel # noqa
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -10,7 +10,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship # noqa
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
