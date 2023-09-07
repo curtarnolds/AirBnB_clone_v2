33bce604efe25de3a049476432217f9328ad7444
curtarnolds <afayedormawuli@gmail.com>
2023-09-07 14:12:53 +0000

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,53 @@
 #!/usr/bin/env bash
-# creates a deployment script for web static
+# Sets up web servers for deployment of `web static`
 
-sudo apt-get -y update
-
+# Install nginx if not already installed
 if ! command -v nginx &> /dev/null; then
-        sudo apt-get -y install nginx
+	sudo apt-get -y update
+	sudo apt-get -y install nginx
+	sudo ufw allow 'Nginx HTTP'
 fi
 
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-echo "Simple content" | sudo tee /data/web_static/releases/test/index.html
+# Create /data/web_static/releases/test, /data/web_static/current, and /data/web_static/shared if they don't exist
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-if [ -L "/data/web_static/current" ]; then
-    rm -R "/data/web_static/current"
-fi
 
-ln -s /data/web_static/releases/test/ /data/web_static/current
+# Create fake HTML to test Nginx configuration
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
+# Create a symlink  /data/web_static/current to /data/web_static/releases/test/.
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Grant ownership of /data/ and it's contents to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-sed -i "s/server_name localhost;/server_name _;/" /etc/nginx/sites-available/default
-sudo sed -i '/server_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Set up nginx to serve static html from /data/web_static/current to hbnb_static
+SERVER_CONFIG=\
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        index index.html;
+        server_name _;
 
+        location /hbnb_static/ {
+        	alias /data/web_static/current/;
+			try_files \$uri \$uri/ =404;
+        }
+		location / {
+                try_files \$uri \$uri/ =404;
+        }
+}"
+
+echo -e "Ceci n'est pas une page\n" > /var/www/html/404.html
+
+# Update config file for nginx
+echo "$SERVER_CONFIG" > /etc/nginx/sites-available/default
+
+# Restart nginx
 sudo service nginx restart
