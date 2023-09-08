27f00c9262fc6deeef77079e67f8baef718efd01
curtarnolds <afayedormawuli@gmail.com>
2023-09-08 05:47:15 +0000

    3. Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -41,14 +41,14 @@ def do_deploy(archive_path=None):
         archive = archive_path.rsplit('/', 1)[1]
         remote_path = archive.rsplit('.', 1)[0]
         wstatic = '/data/web_static'
-        run(f'mkdir -p {wstatic}/releases/{remote_path}')
+        run(f'mkdir -p {wstatic}/releases/{remote_path}/')
         run(f'tar -xzf /tmp/{archive} -C \
-                          {wstatic}/releases/{remote_path}')
+                          {wstatic}/releases/{remote_path}/')
         run(f'mv -f {wstatic}/releases/{remote_path}/web_static/* \
-                       {wstatic}/releases/{remote_path}')
+                       {wstatic}/releases/{remote_path}/')
         run(f'rm -rf {wstatic}/releases/{remote_path}/web_static/ \
                      /tmp/{archive} {wstatic}/current')
-        run(f'ln -s {wstatic}/releases/{remote_path} {wstatic}/current')
+        run(f'ln -s {wstatic}/releases/{remote_path}/ {wstatic}/current')
         puts('New version deployed!')
         return True
     return False
