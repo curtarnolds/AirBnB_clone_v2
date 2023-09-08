0a8cee3e46c44c34a68406d2bde52a572762958c
curtarnolds <afayedormawuli@gmail.com>
2023-09-08 12:01:06 +0000

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -35,15 +35,15 @@ def do_deploy(archive_path):
         newest_version = "/data/web_static/releases/" + archived_file[:-4]
         archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
+        sudo("sudo mkdir -p {}".format(newest_version))
+        sudo("sudo tar -xzf {} -C {}/".format(archived_file,
                                              newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
+        sudo("sudo rm {}".format(archived_file))
+        sudo("sudo mv {}/web_static/* {}".format(newest_version,
                                                 newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+        sudo("sudo rm -rf {}/web_static".format(newest_version))
+        sudo("sudo rm -rf /data/web_static/current")
+        sudo("sudo ln -s {} /data/web_static/current".format(newest_version))
 
         print("New version deployed!")
         return True
