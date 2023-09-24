75444a8f5599d3378edcbfe04725efd4e5aa05ed
curtarnolds <afayedormawuli@gmail.com>
2023-09-24 12:21:41 +0000

    1. HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A simple Flask application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """Return 'Hello HBNB'"""
+    return "Hello HBNB"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Return 'HBNB'"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
