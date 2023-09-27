cf5cbe8710e0b5595fe86ffebda0a64a8ccf3cd6
curtarnolds <afayedormawuli@gmail.com>
2023-09-27 12:57:17 +0000

    8. List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""A simple Flask application"""
+from flask import Flask, render_template, g
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove current SQLAlchemy Session during teardown"""
+    db = g.pop('db', None)
+    if db is not None:
+        storage.close()
+
+
+@app.route('/')
+def hello_hbnb():
+    """Return 'Hello HBNB'"""
+    return "Hello HBNB"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Return 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def text(text):
+    """Return simple text"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python')
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """Return simple text"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """Return simple text"""
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>')
+def number_temp(n):
+    """Render a template"""
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """Render a template"""
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+@app.route('/states_list')
+def states_list():
+    """Display HTML of list of states"""
+    storage.reload()
+    states_obj = storage.all('State')
+    states_list = [key for key in states_obj.keys()]
+    return render_template('7-states_list.html', states=states_list)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
