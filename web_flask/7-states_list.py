375865f1afd0d982d5d31dbcadc93db96bc13a43
curtarnolds <afayedormawuli@gmail.com>
2023-09-27 13:29:11 +0000

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,13 @@ from flask import Flask, render_template, g
 from models import storage
 
 
+classes = [
+        'BaseModel', 'User', 'Place',
+        'State', 'City', 'Amenity',
+        'Review'
+    ]
+
+
 app = Flask(__name__)
 
 
@@ -63,7 +70,7 @@ def number_odd_or_even(n):
 def states_list():
     """Display HTML of list of states"""
     storage.reload()
-    states_obj = storage.all('State')
+    states_obj = storage.all('States')
     states_list = [key for key in states_obj.values()]
     return render_template('7-states_list.html', states=states_list)
 
